8340781f471bdac3b4a10b1af6ee878ae122b1b3
mlwelker <nutria.watts.0g@icloud.com>
2023-06-23 14:40:01 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Michael Welker on 2023-06-23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
